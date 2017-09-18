60d84616531cd29e3be98d0dee15ffc1afa73c35
DonalMcGahon <dmcgahon1234@hotmail.com>
2017-09-18 22:07:08 +0100

    Palindrome.py

--- Palindrome.Q7/Palindrome.py
+++ Palindrome.Q7/Palindrome.py
@@ -1,2 +1,10 @@
 # Ask user to input a string
 user_string = str(input('Enter a string to see if it is palindrome or not: '))
+# This is used to reverse the string
+string_rev = reversed(user_string)
+
+# Check to see if the string is equal to itself in reverse
+if list(user_string) == list(string_rev):
+   print("It is palindrome")
+else:
+   print("It is not palindrome")
