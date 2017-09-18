b1be9c8ce95f1be713a0852aa117b803fe8ac9f6
DonalMcGahon <dmcgahon1234@hotmail.com>
2017-09-18 22:21:42 +0100

    Question 8 complete

--- Sorted list.Q8/SortedList.py
+++ Sorted list.Q8/SortedList.py
@@ -1,5 +1,14 @@
-list1 = [1,4,6]
-list2 = [2,3,5]
+# Adapted from - https://stackoverflow.com/questions/29615274/user-input-integer-list
 
+# Ask user to enter a list of numbers
+list1 = input("Please enter a list of numbers separated by a single space only: ")
+list1 = list1.split(' ')
+
+# Ask user to enter a list of numbers
+list2 = input("Please enter a list of numbers separated by a single space only: ")
+list2 = list2.split(' ')
+
+# .extend allows for the two lists to join together
 list1.extend(list2)
+# sorted sorts the list
 print(sorted(list1))
