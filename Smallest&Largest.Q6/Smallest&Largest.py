2a1802a5d2926ceaf39729f4d8ad0e9561f62392
DonalMcGahon <dmcgahon1234@hotmail.com>
2017-09-18 21:51:12 +0100

    Question 6 complete

--- Smallest&Largest.Q6/Smallest&Largest.py
+++ Smallest&Largest.Q6/Smallest&Largest.py
@@ -1,9 +1,12 @@
-import random
-
+# Create an empty list
 lst = []
+# Ask user how many numbers they would like in the list
+num = int(input('How many numbers: '))
 
-for x in range(6):
-    numbers = int(input("Enter Number: "))
+# For the amount of numbers the user wants in the list, ask them to enter a number for each digit in the list
+for n in range(num):
+    numbers = int(input('Enter number '))
+    # .append adds the numbers to the list
     lst.append(numbers)
-
-    print("Largest number is: ", max(lst), "\n while the smallest number is: ", min(lst))
+    # Print out the Lagest and Smallest numbers
+print("Maximum element in the list is :", max(lst), "\nMinimum element in the list is :", min(lst))
