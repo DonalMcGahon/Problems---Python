115ec63c623b39861db2509fc9ba78b3e5ee8cf6
DonalMcGahon <dmcgahon1234@hotmail.com>
2017-09-19 12:18:32 +0100

    Q9 + Q 10

--- Newton’s method.Q9/Newton.py
+++ Newton’s method.Q9/Newton.py
@@ -1,6 +1,20 @@
-z = int(input('Enter a number to find square root: '))
+"""z = int(input('Enter a number to find square root: '))
 x = 0
 
 z_next = z - ((z*z - x) / (2 * z))
 
-print(z_next)
+print(z_next)"""
+
+# Ask user to enter number
+z = int(input('Enter a number to find square root: '))
+
+# Muliply user number by 0.5
+squareroot = 0.5 * z
+x = 0.5 * (squareroot + z/squareroot)
+# While squareroot is not equal to x, repeat x
+while x != squareroot:
+    squareroot = x
+    x = 0.5 * (squareroot + z/squareroot)
+    
+    # print square root
+print(squareroot)
--- Reverse String.Q10/ReverseString.py
+++ Reverse String.Q10/ReverseString.py
@@ -1,5 +1,7 @@
 user_string = str(input('Enter a string: '))
 
-user_string = reversed(user_string)
+user_string_rev = reversed(user_string)
 
-print(user_string)
+user_string_rev.join(reverse(my_string))
+
+print(user_string_rev)
