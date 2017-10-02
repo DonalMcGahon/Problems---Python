a72298db12e619663cf7bce6ce1465623fab364c
DonalMcGahon <dmcgahon1234@hotmail.com>
2017-10-02 11:51:01 +0100

    Q3 Complete

--- FizzBuzz.Q3/FizzBuzz.py
+++ FizzBuzz.Q3/FizzBuzz.py
@@ -1,9 +1,16 @@
+# Create a range from 1 to 100
 for x in range(1, 101):
     result = ""
     result1 = ""
     result2 = ""
+    print(x)
+    # if number is divisible by 3
     if not x % 3:
-        result1 += "Fizz"
+        result += "Fizz"
+        # if number is divisible by 5
     if not x % 5:
-        result += "Buzz"
-        print(x, result, result1, result2)
+        result1 += "Buzz"
+        # if number is divisible by 3 and 5
+    if not x % 3 and 5:
+        result2 += "FizzBuzz"
+        print(result, result1, result2)
